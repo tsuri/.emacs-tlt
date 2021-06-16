d12a357eb18c238f57a77266f58d0e1d1a9fba4a
Maurizio Vitale <mav@polymath-solutions.com>
2021-06-15 20:43:16 -0400

    Add early-init.el

---
+++ early-init.el
@@ -0,0 +1,10 @@
+(setq package-enable-at-startup nil)
+(setq package-archives nil)
+
+(advice-add #'x-apply-session-resources :override #'ignore)
+
+(setq frame-inhibit-implied-resize t)
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars . 0) default-frame-alist)
